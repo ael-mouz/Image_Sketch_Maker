836ff338682dc570ec2d72b4cab32fc6c105edea
ael-mouz <elmouzammil.ock@gmail.com>
2023-03-13 06:11:39 +0100

    commit2

--- Image_Sketch_Maker.py
+++ Image_Sketch_Maker.py
@@ -1,7 +1,12 @@
 import cv2
+import os
 # pip install opencv-python
 # sudo apt-get install libgl1-mesa-glx
 # export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
+
+# Create a new folder to store the sketches
+if not os.path.exists("sketches"):
+    os.mkdir("sketches")
 while True:
 	# Ask the user for the filename of the input image
 	input_filename = input("Enter the input image (or 'q' to quit): ")
@@ -21,7 +26,7 @@ while True:
 	# Divide the grayscale image by the inverted blurred image
 	sketch = cv2.divide(grey_img, invertedblur, scale=256)
 	# Generate a filename for the output sketch
-	output_filename = input_filename.split(".")[0] + "_sketch.png"
+	output_filename = os.path.join("sketches", os.path.basename(input_filename).split(".")[0] + "_sketch.png")
 	# Save the sketch to a file
 	cv2.imwrite(output_filename, sketch)
 	# Print a message to confirm that the sketch was saved
